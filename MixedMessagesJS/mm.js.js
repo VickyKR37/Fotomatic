540a83005bf5a477b8762f1d6f62d256eb651120
Vicky Rai <vickyks@hotmail.com>
2021-02-04 19:25:41 +0000

    initial commit as added a method to randomly select quotes

--- MixedMessagesJS/mm.js.js
+++ MixedMessagesJS/mm.js.js
@@ -1,11 +1,14 @@
 mm.js
 let bibleQuotes = (opening, quote, closing) => {
+return { 
     opening: opening,
     quote: quote,
-    closing: closing
-};
-
-
+    closing: closing,
+    randomSelection() {
 let openers = 'God says '
 let quotes = ['For we walk by faith, not sight.', 'It is God who arms me with strength and makes my ways perfect.', 'The Lord gives strength to his people; the lord blesses his people with peace.']
 let closers = ['Pray today.', 'Lean in Him.']
+        
+}
+}
+}
