6d706b597a33f1f3bad1f75e63ae3cf17d2cb502
Vicky Rai <vickyks@hotmail.com>
2021-02-04 19:33:17 +0000

    Changed  variable names and added a for loop

--- MixedMessagesJS/mm.js.js
+++ MixedMessagesJS/mm.js.js
@@ -5,10 +5,12 @@ return {
     quote: quote,
     closing: closing,
     randomSelection() {
-let openers = 'God says '
+let opener = 'God says '
 let quotes = ['For we walk by faith, not sight.', 'It is God who arms me with strength and makes my ways perfect.', 'The Lord gives strength to his people; the lord blesses his people with peace.']
-let closers = ['Pray today.', 'Lean in Him.']
-        
+let closer = ['Lean on Him.']
+        for (let i = 0; i < quotes.length; i++) {
+            console.log(opener + quotes[i] + closer)
+        }
 }
 }
 }
