9a2cfb21433bcc9d3fd0202e274d441ddb4326fd
Vicky Rai <vickyks@hotmail.com>
2021-02-04 19:18:32 +0000

    initial commit

---
+++ MixedMessagesJS/mm.js.js
@@ -0,0 +1,11 @@
+mm.js
+let bibleQuotes = (opening, quote, closing) => {
+    opening: opening,
+    quote: quote,
+    closing: closing
+};
+
+
+let openers = 'God says '
+let quotes = ['For we walk by faith, not sight.', 'It is God who arms me with strength and makes my ways perfect.', 'The Lord gives strength to his people; the lord blesses his people with peace.']
+let closers = ['Pray today.', 'Lean in Him.']
